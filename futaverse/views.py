b53d2455dd08e3dbd1dda7971ca7ea8b8bc7776b
Ceejay-777 <covenantcrackslord01@gmail.com>
2026-08-17 07:18:14 +0100

    fix: health check

--- futaverse/views.py
+++ futaverse/views.py
@@ -1,9 +1,12 @@
+from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
+
 class PublicGenericAPIView(generics.GenericAPIView):
-    authentication_classes = []  
+    authentication_classes = []
     permission_classes = [AllowAny]
 
+
 def health_check(request):
-    return Response({"status": "ok"})
+    return JsonResponse({"status": "ok"})
